dab5fcb0883ac4d0f86590b15db948d08feb810e
Squibs <z.r.holman+github@gmail.com>
2021-01-28 14:49:43 -0700

    Show list of streams, edit/delete, create stream.

--- 9. streams/src/app/views/pages/StreamList.tsx
+++ 9. streams/src/app/views/pages/StreamList.tsx
@@ -1,11 +1,17 @@
 import React, { useEffect } from 'react';
 import { connect, ConnectedProps } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { streamsOperations } from '../../state/ducks/streams';
+import { StreamsState } from '../../state/ducks/streams/types';
 import { AppState } from '../../state/store';
 
 /* ------------------------------ Redux Config ------------------------------ */
 
-const mapStateToProps = (state: AppState) => ({});
+const mapStateToProps = (state: AppState) => ({
+  streams: Object.values(state.streams.streams),
+  currentUserId: state.session.auth.userId,
+  isSignedIn: state.session.auth.isSignedIn,
+});
 
 const mapDispatchToProps = { fetchStreams: streamsOperations.fetchStreams };
 
@@ -20,12 +26,69 @@ type Props = PropsFromRedux;
 /* -------------------------------- Component ------------------------------- */
 
 // home/index
-const StreamList: React.FC<Props> = ({ fetchStreams }) => {
+const StreamList: React.FC<Props> = ({
+  fetchStreams,
+  streams,
+  currentUserId,
+  isSignedIn,
+}: Props) => {
   useEffect(() => {
     fetchStreams();
   }, [fetchStreams]);
 
-  return <>StreamList</>;
+  // delete and edit buttons; user has admin privileges over this stream
+  const renderAdmin = (stream: StreamsState) => {
+    if (stream.userId === currentUserId) {
+      return (
+        <div className="right floated content">
+          <button type="button" className="ui button primary">
+            Edit
+          </button>
+          <button type="button" className="ui button negative">
+            Delete
+          </button>
+        </div>
+      );
+    }
+
+    return null;
+  };
+
+  const renderList = () => {
+    return [...streams].map((stream) => {
+      return (
+        <div className="item" key={stream.id}>
+          {renderAdmin(stream)}
+          <i className="large middle aligned icon camera" />
+          <div className="content">
+            <Link to={`/streams/${stream.id}`}>{stream.title}</Link>
+            <div className="description">{stream.description}</div>
+          </div>
+        </div>
+      );
+    });
+  };
+
+  // show create stream button if user is logged in
+  const renderCreate = () => {
+    if (isSignedIn) {
+      return (
+        <Link to="/streams/new" className="ui button primary right floated">
+          Create Stream
+        </Link>
+      );
+    }
+
+    return null;
+  };
+
+  return (
+    <>
+      <h2>Streams</h2>
+      <div className="ui celled list">{renderList()}</div>
+      {renderCreate()}
+    </>
+  );
 };
 
 export default connector(StreamList);
