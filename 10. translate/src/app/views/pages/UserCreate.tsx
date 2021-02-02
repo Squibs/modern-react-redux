fc1354d1e122bccc0dabbfc8ba1e517cd5c168e3
Squibs <z.r.holman+github@gmail.com>
2021-02-01 23:12:24 -0700

    Note about props.

--- 10. translate/src/app/views/pages/UserCreate.tsx
+++ 10. translate/src/app/views/pages/UserCreate.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Button, Field } from '../components';
 
+// has no knowledge of the props being passed to its child components
 const UserCreate: React.FC = () => {
   return (
     <div className="ui form">
